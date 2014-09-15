727832e384a1aed00f82e48e07430553e9d3d90f
Trevin Hofmann <trevinhofmann@gmail.com>
2014-09-15 16:26:18 -0500

    Add information to address page
    Address page now displays amounts received based on data grabbed from
    the outputs index database. Woo!

--- controllers/address.js
+++ controllers/address.js
@@ -8,15 +8,68 @@ exports.render = function (req, res) {
   }
   var scriptPubKey = (new Address(addr)).getScriptPubKey()['buffer'].toString('hex');
   var scriptPubKey_hash = crypto.createHash('sha256').update(scriptPubKey).digest('hex');
-  database.get('SELECT HEX(txid) as txid, vout, claims FROM outputs WHERE scriptPubKey_hash=X\''+scriptPubKey_hash+'\'', function(rows){
-    var address = {
-      address: addr,
-      transactions: rows,
-      confirmedReceived: 0,
-      unonfirmedReceived: 0,
-      confirmedPossiblyReceived: 0,
-      unconfirmedPossiblyReceived: 0
-    };
-    res.render('address', {address: address});
+  database.get('SELECT HEX(txid) as txid, vout FROM outputs WHERE scriptPubKey_hash=X\''+scriptPubKey_hash+'\'', function(rows){
+    var outputs = rows;
+    getTransactions(outputs, function(txs){
+      var confirmedReceived = 0;
+      var unconfirmedReceived = 0;
+      var confirmedPossiblyReceived = 0;
+      var unconfirmedPossiblyReceived = 0;
+      for (var tx in txs){
+        tx = txs[tx];
+        if (tx.confirmations > config.minConfirmations){
+          if (tx.amount > Math.max(confirmedReceived, unconfirmedReceived)){
+            confirmedReceived = tx.amount;
+            unconfirmedReceived = 0;
+          }
+          else{
+            confirmedPossiblyReceived += tx.amount;
+          }
+        }
+        else{
+          if (confirmedReceived > 0){
+            unconfirmedPossiblyReceived += tx.amount;
+          }
+          else if (unconfirmedReceived > 0 && tx.amount > unconfirmedReceived){
+            unconfirmedPossiblyReceived = unconfirmedReceived;
+            unconfirmedReceived = tx.amount;
+          }
+          else{
+            unconfirmedReceived = tx.amount;
+          }
+        }
+      }
+      var address = {
+        address: addr,
+        transactions: txs,
+        confirmedReceived: confirmedReceived,
+        unconfirmedReceived: unconfirmedReceived,
+        confirmedPossiblyReceived: confirmedPossiblyReceived,
+        unconfirmedPossiblyReceived: unconfirmedPossiblyReceived
+      };
+      res.render('address', {address: address});
+    });
   });
 };
+
+function getTransactions(outputs, callback){
+  var fetched = 0;
+  var txs = {};
+  for (var output in outputs){
+    rpc.getRawTransaction(outputs[output].txid, 1, function(err, ret){
+      var tx = ret.result;
+      var txid = outputs[output].txid;
+      if (typeof txs[txid] == 'undefined'){
+        tx.confirmations = tx.confirmations || 0;
+        tx.isCoinbase = (typeof (tx["vin"][0]["coinbase"]) != 'undefined');
+        tx.amount = 0;
+        txs[txid] = tx;
+      }
+      txs[txid].amount += tx.vout[outputs[output].vout].value;
+      fetched ++;
+      if (fetched == outputs.length){
+        callback(txs);
+      }
+    });
+  }
+}
