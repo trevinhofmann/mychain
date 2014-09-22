7848c5aa7db064233ccfa30e89ad1567fed85fa8
Trevin Hofmann <trevinhofmann@gmail.com>
2014-09-22 16:31:50 -0500

    Clean up database.js
    It looks much better.

--- database.js
+++ database.js
@@ -28,134 +28,189 @@ var chainHeight = 0;
 
 // This opens the connection to the SQL database and creates the database/table structure if it's
 // not already present.
-// TODO: Try to simplify this. Use helper functions? Use returns instead of deep nesting?
 function connectToDatabase(){
   connection = mysql.createConnection(config.mysql);
-  connection.connect(function(err){
-    console.log(err ? 'Failed to connect to MySQL database.' : 'Connected to MySQL database.');
-    if (!err){
-      connection.query('CREATE DATABASE IF NOT EXISTS mychain', function (err, result){
-        connection.query('USE mychain', function (err, result){
-          for (var table in tables){
-            connection.query('CREATE TABLE IF NOT EXISTS '+table+' ('+tables[table].join(',')+')');
+  connection.connect(handleDatabaseConnection);
+  connection.on('error', handleDatabaseError);
+}
+
+// On connection to the database, logs a success/failure message to the console and begins creating the
+// database structure.
+function handleDatabaseConnection(err){
+  console.log(err ? 'Failed to connect to MySQL database.' : 'Connected to MySQL database.');
+  if (!err){
+    createDatabase();
+  }
+}
+
+// Creates the mychain database and its table structure if not already set up. Begins updating the
+// database once it is ready.
+function createDatabase(){
+  connection.query('CREATE DATABASE IF NOT EXISTS mychain', function (err, result){
+    connection.query('USE mychain', function (err, result){
+      var remaining = 0;
+      for (var table in tables){
+        remaining ++;
+      }
+      for (var table in tables){
+        connection.query('CREATE TABLE IF NOT EXISTS '+table+' ('+tables[table].join(',')+')', function(){
+          remaining --;
+          if (remaining == 0){
+            update();
           }
-          setTimeout(update, 2000);
         });
-      });
-    }
+      }
+    });
   });
-  connection.on('error', function(err){
-    if (err.code === 'PROTOCOL_CONNECTION_LOST'){
-      console.log("Database error. Reconnecting.");
-      connectToDatabase();
-    }
-    else{
-      throw err;
-    }
-  });
+}
+
+// Handle a database error. If connection is lost, attempt to salvage the situation by reconnecting.
+function handleDatabaseError(err){
+  if (err.code === 'PROTOCOL_CONNECTION_LOST'){
+    console.log("Database error. Reconnecting.");
+    connectToDatabase();
+  }
+  else{
+    throw err;
+  }
 }
 
 // This accepts an array of transaction id's, gets their raw transaction data from the RPC, and
 // stores it in the outputs table of the SQL database.
-// TODO: This is ugly and has way too much nesting. Use helper functions and returns instead.
-function addTransactions(txs){
+function addTransactions(txs, callback){
   if (txs.length > 0){
     rpc.getRawTransaction(txs[0], 1, function(err, ret){
       if (err){
-        return setTimeout(addTransactions, 1000, txs);
+        setTimeout(function(){
+          addTransactions(txs, callback);
+        }, 1000);
+        return;
       }
-      var tx = ret.result;
-      var outputsInserted = 0;
-      for (var output in tx.vout){
-        var scriptPubKeyHash = crypto.createHash('sha256').update(tx.vout[output].scriptPubKey.hex).digest('hex');
-        var query = 'INSERT INTO outputs (scriptPubKey_hash, txid, vout) VALUES (X\''+scriptPubKeyHash+'\', X\''+tx.txid+'\', '+output+')';
-        connection.query(query, function(err, result){
-          if (err){
-            console.log('Database error: Failed to insert an output.');
-          }
-          outputsInserted ++;
-          if (outputsInserted == tx.vout.length){
-            if (typeof (tx.vin[0]["coinbase"]) == 'undefined'){
-              var inputsInserted = 0;
-              for (var input in tx.vin){
-                var query = 'UPDATE outputs SET claims = concat_ws(\'\', claims, \','+tx.txid+'/'+input+'\') WHERE txid=X\''+tx.vin[input].txid+'\' AND vout='+tx.vin[input].vout;
-                connection.query(query, function(err, result){
-                  if (err){
-                    console.log('Database error: Failed to add input claim to an output.');
-                  }
-                  inputsInserted ++;
-                  if (inputsInserted == tx.vin.length){
-                    txs.shift();
-                    setTimeout(addTransactions, 10, txs);
-                  }
-                });
-              }
-            }
-            else{
-              txs.shift();
-              setTimeout(addTransactions, 10, txs);
-            }
-          }
-        });
-      }
+      addTransaction(ret.result, function(){
+        txs.shift();
+        addTransactions(txs, callback);
+      });
     });
+    return;
   }
-  else{
-    var query = 'UPDATE vars SET block_height_checked='+heightChecked;
+  blockAdded(callback);
+}
+
+// Update 'block_height_checked' in vars table.
+function blockAdded(callback){
+  var query = 'UPDATE vars SET block_height_checked='+heightChecked;
+  connection.query(query, function(err, result){
+    if (err){
+      console.log('Database error: Failed to update block_height_checked.');
+    }
+    callback();
+  });
+}
+
+// Adds a single transaction's data to the outputs database.
+function addTransaction(tx, callback){
+  addOutputs(tx, function(){
+    addInputs(tx, function(){
+      callback();
+    });
+  });
+}
+
+// Adds a transaction's outputs to the database.
+function addOutputs(tx, callback){
+  var outputsInserted = 0;
+  for (var output in tx.vout){
+    var scriptPubKeyHash = crypto.createHash('sha256').update(tx.vout[output].scriptPubKey.hex).digest('hex');
+    var query = 'INSERT INTO outputs (scriptPubKey_hash, txid, vout) VALUES (X\''+scriptPubKeyHash+'\', X\''+tx.txid+'\', '+output+')';
     connection.query(query, function(err, result){
       if (err){
-        console.log('Database error: Failed to update block_height_checked.');
+        console.log('Database error: Failed to insert an output.');
       }
-      setTimeout(update, 20);
+      outputsInserted ++;
+      if (outputsInserted == tx.vout.length){
+        callback();
+      }
     });
   }
+}
+
+// Adds a transaction's inputs to the database.
+function addInputs(tx, callback){
+  if (typeof (tx.vin[0]["coinbase"]) == 'undefined'){
+    var inputsInserted = 0;
+    for (var input in tx.vin){
+      var query = 'UPDATE outputs SET claims = concat_ws(\'\', claims, \','+tx.txid+'/'+input+'\') WHERE txid=X\''+tx.vin[input].txid+'\' AND vout='+tx.vin[input].vout;
+      connection.query(query, function(err, result){
+        if (err){
+          console.log('Database error: Failed to add input claim to an output.');
+        }
+        inputsInserted ++;
+        if (inputsInserted == tx.vin.length){
+          callback();
+        }
+      });
+    }
+    return;
+  }
+  callback();
 }
 
 // This function is run to acquire data about the next block and insert it into the outputs
 // table of the SQL database.
 function update(){
-  if (heightChecked > -1){
-    if (heightChecked >= chainHeight){
-      rpc.getBlockCount(function(err, ret){
-        chainHeight = ret.result - 1;
-        return setTimeout(update, 250);
+  if (heightChecked < 0){
+    loadHeightChecked(function(){
+      update();
+    });
+    return;
+  }
+  if (heightChecked >= chainHeight){
+    rpc.getBlockCount(function(err, ret){
+      chainHeight = ret.result - 1;
+      update();
+    });
+    return;
+  }
+  processNextBlock(function(){
+    setTimeout(update, 5);
+  });
+}
+
+// Loads the next block and stores its data in the database.
+function processNextBlock(callback){
+  rpc.getBlockHash(heightChecked+1, function(err, ret){
+    if (err){
+      return setTimeout(update, 1000);
+    }
+    rpc.getBlock(ret.result, function(err, ret){
+      if (err){
+        return setTimeout(update, 1000);
+      }
+      heightChecked ++;
+      console.log('Adding transactions from block height '+heightChecked);
+      addTransactions(ret.result.tx, callback);
+    });
+  });
+}
+
+// Loads the height checked from database.
+function loadHeightChecked(callback){
+  connection.query('SELECT block_height_checked FROM vars', function(err, rows){
+    if (rows.length == 0){
+      var query = 'INSERT INTO vars (block_height_checked) VALUES (0)';
+      connection.query(query, function(err, result){
+        heightChecked = 0;
+        callback();
       });
+    }
+    else if (rows.length == 1){
+      heightChecked = rows[0]['block_height_checked'];
+      callback();
     }
     else{
-      rpc.getBlockHash(heightChecked+1, function(err, ret){
-        if (err){
-          return setTimeout(update, 1000);
-        }
-        rpc.getBlock(ret.result, function(err, ret){
-          if (err){
-            return setTimeout(update, 1000);
-          }
-          heightChecked ++;
-          console.log('Adding transactions from block height '+heightChecked);
-          addTransactions(ret.result.tx);
-        });
-      });
+      console.log('There is a problem with the database \'vars\' table. Multiple rows exist, and there should be only one.');
     }
-  }
-  else{
-    connection.query('SELECT block_height_checked FROM vars', function(err, rows){
-      if (err) throw err;
-      if (rows.length == 0){
-        var query = 'INSERT INTO vars (block_height_checked) VALUES (0)';
-        connection.query(query, function(err, result){
-          heightChecked = 0;
-          return setTimeout(update, 10);
-        });
-      }
-      else if (rows.length == 1){
-        heightChecked = rows[0]['block_height_checked'];
-        return setTimeout(update, 10);
-      }
-      else{
-        console.log('There is a problem with the database \'vars\' table. Multiple rows exist, and there should be only one.');
-      }
-    });
-  }
+  });
 }
 
 // Process a SELECT query.
