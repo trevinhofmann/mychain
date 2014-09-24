3717c3b022da1c2095c750a3c388991d9090e9fb
Trevin Hofmann <trevinhofmann@gmail.com>
2014-09-24 11:46:22 -0500

    Continue simplifying api.js
    This should be good enough for now.

--- controllers/api.js
+++ controllers/api.js
@@ -13,11 +13,9 @@ function renderIndex(req, res){
 }
 
 // Render the data result for a given API call.
-// TODO: Break this up with a different function for each call.
 function renderResult(req, res){
   var command = req.params.command;
   var params = req.query;
-  var response = {};
   if (command == 'blockhash'){
     renderBlock(req, res)
   }
@@ -75,79 +73,24 @@ function renderTransaction(req, res){
 // Render the API result for an address.
 function renderAddress(req, res){
   var params = req.query;
-  var response = {};
+  var addr = params.address;
   if (typeof params.address == 'undefined'){
     renderError(req, res, 'Required parameter \'address\' is missing.');
     return;
   }
-  var addr = params.address;
   if (!(new Address(addr)).isValid()){
     renderError(req, res, 'Address was invalid.');
     return;
   }
-  var scriptPubKey = (new Address(addr)).getScriptPubKey()['buffer'].toString('hex');
-  var scriptPubKey_hash = crypto.createHash('sha256').update(scriptPubKey).digest('hex');
-  database.get('SELECT HEX(txid) as txid, vout FROM outputs WHERE scriptPubKey_hash=X\''+scriptPubKey_hash+'\'', function(rows){
-    var outputs = rows;
-    getTransactions(outputs, {}, function(txs){
-      var confirmedReceived = 0;
-      var unconfirmedReceived = 0;
-      var confirmedPossiblyReceived = 0;
-      var unconfirmedPossiblyReceived = 0;
-      for (var tx in txs){
-        tx = txs[tx];
-        if (tx.confirmations > config.minConfirmations){
-          if (tx.amount > Math.max(confirmedReceived, unconfirmedReceived)){
-            confirmedReceived = tx.amount;
-            unconfirmedReceived = 0;
-          }
-          else{
-            confirmedPossiblyReceived += tx.amount;
-          }
-        }
-        else{
-          if (confirmedReceived > 0){
-            unconfirmedPossiblyReceived += tx.amount;
-          }
-          else if (unconfirmedReceived > 0 && tx.amount > unconfirmedReceived){
-            unconfirmedPossiblyReceived = unconfirmedReceived;
-            unconfirmedReceived = tx.amount;
-          }
-          else{
-            unconfirmedReceived = tx.amount;
-          }
-        }
-      }
-      for (var key in txs){
-        delete txs[key].vin;
-        delete txs[key].vout;
-        delete txs[key].scriptSig;
-        delete txs[key].hex;
-        delete txs[key].txid;
-        delete txs[key].version;
-        delete txs[key].locktime;
-        delete txs[key].blocktime;
-        delete txs[key].blockhash;
-        delete txs[key].time;
-        delete txs[key].isCoinbase;
-        txs[key.toLowerCase()] = txs[key];
-        delete txs[key];
-      }
-      var address = {
-        address: addr,
-        confirmedReceived: confirmedReceived,
-        unconfirmedReceived: unconfirmedReceived,
-        confirmedPossiblyReceived: confirmedPossiblyReceived,
-        unconfirmedPossiblyReceived: unconfirmedPossiblyReceived,
-        transactions: txs
-      };
-      response.status = 'ok';
-      response.data = address;
-      return res.json(response);
-    });
+  getAddressData(addr, function(address){
+    var response = {};
+    response.status = 'ok';
+    response.data = address;
+    return res.json(response);
   });
 }
 
+// Render an API error message.
 function renderError(req, res, message){
   var response = {};
   response.status = 'error';
@@ -176,4 +119,81 @@ function getTransactions(outputs, txs, callback){
   else{
     callback(txs);
   }
+}
+
+// Remove transaction data that is not relevant to an address transaction receipt.
+function removeExtraTransactionData(txs){
+  for (var key in txs){
+    delete txs[key].vin;
+    delete txs[key].vout;
+    delete txs[key].scriptSig;
+    delete txs[key].hex;
+    delete txs[key].txid;
+    delete txs[key].version;
+    delete txs[key].locktime;
+    delete txs[key].blocktime;
+    delete txs[key].blockhash;
+    delete txs[key].time;
+    delete txs[key].isCoinbase;
+    txs[key.toLowerCase()] = txs[key];
+    delete txs[key];
+  }
+  return txs;
+}
+
+// Calculates the amount received by an address.
+function calculateReceived(txs){
+  var received = {
+    'confirmed': 0,
+    'unconfirmed': 0,
+    'confirmedPossibly': 0,
+    'unconfirmedPossibly': 0
+  };
+  for (var tx in txs){
+    tx = txs[tx];
+    if (tx.confirmations > config.minConfirmations){
+      if (tx.amount > Math.max(received['confirmed'], received['unconfirmed'])){
+        received['confirmed'] = tx.amount;
+        received['unconfirmed'] = 0;
+      }
+      else{
+        received['confirmedPossibly'] += tx.amount;
+      }
+    }
+    else{
+      if (received['confirmed'] > 0){
+        received['unconfirmedPossibly'] += tx.amount;
+      }
+      else if (received['unconfirmed'] > 0 && tx.amount > received['unconfirmed']){
+        received['unconfirmedPossibly'] = received['unconfirmed'];
+        received['unconfirmed'] = tx.amount;
+      }
+      else{
+        received['unconfirmed'] = tx.amount;
+      }
+    }
+  }
+  return received;
+}
+
+// Get data for an address.
+function getAddressData(addr, callback){
+  var scriptPubKey = (new Address(addr)).getScriptPubKey()['buffer'].toString('hex');
+  var scriptPubKey_hash = crypto.createHash('sha256').update(scriptPubKey).digest('hex');
+  database.get('SELECT HEX(txid) as txid, vout FROM outputs WHERE scriptPubKey_hash=X\''+scriptPubKey_hash+'\'', function(rows){
+    var outputs = rows;
+    getTransactions(outputs, {}, function(txs){
+      var received = calculateReceived(txs);
+      txs = removeExtraTransactionData(txs);
+      var address = {
+        address: addr,
+        confirmedReceived: received['confirmed'],
+        unconfirmedReceived: received['unconfirmed'],
+        confirmedPossiblyReceived: received['confirmedPossibly'],
+        unconfirmedPossiblyReceived: received['unconfirmedPossibly'],
+        transactions: txs
+      };
+      callback(address);
+    });
+  });
 }
