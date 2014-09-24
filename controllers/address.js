7cebe60e87506e0f2b0a5cfe18f6d8b1643acfe9
Trevin Hofmann <trevinhofmann@gmail.com>
2014-09-24 11:47:23 -0500

    Simplify address.js
    Definitely need to create models with an address. The address.js and
    api.js rendering scripts share a lot of functions that should be in a
    single address script.

--- controllers/address.js
+++ controllers/address.js
@@ -1,72 +1,19 @@
 'use strict';
 
+// Render an address page for a given address.
 exports.render = function (req, res) {
   var addr = req.params.address;
   if (!(new Address(addr)).isValid()){
     res.render('index', {error: 'Address was invalid: '+addr});
     return;
   }
-  var scriptPubKey = (new Address(addr)).getScriptPubKey()['buffer'].toString('hex');
-  var scriptPubKey_hash = crypto.createHash('sha256').update(scriptPubKey).digest('hex');
-  database.get('SELECT HEX(txid) as txid, vout FROM outputs WHERE scriptPubKey_hash=X\''+scriptPubKey_hash+'\'', function(rows){
-    var outputs = rows;
-    getTransactions(outputs, {}, function(txs){
-      var confirmedReceived = 0;
-      var unconfirmedReceived = 0;
-      var confirmedPossiblyReceived = 0;
-      var unconfirmedPossiblyReceived = 0;
-      for (var tx in txs){
-        tx = txs[tx];
-        if (tx.confirmations > config.minConfirmations){
-          if (tx.amount > Math.max(confirmedReceived, unconfirmedReceived)){
-            confirmedReceived = tx.amount;
-            unconfirmedReceived = 0;
-          }
-          else{
-            confirmedPossiblyReceived += tx.amount;
-          }
-        }
-        else{
-          if (confirmedReceived > 0){
-            unconfirmedPossiblyReceived += tx.amount;
-          }
-          else if (unconfirmedReceived > 0 && tx.amount > unconfirmedReceived){
-            unconfirmedPossiblyReceived = unconfirmedReceived;
-            unconfirmedReceived = tx.amount;
-          }
-          else{
-            unconfirmedReceived = tx.amount;
-          }
-        }
-      }
-      for (var key in txs){
-        delete txs[key].vin;
-        delete txs[key].vout;
-        delete txs[key].scriptSig;
-        delete txs[key].hex;
-        delete txs[key].txid;
-        delete txs[key].version;
-        delete txs[key].locktime;
-        delete txs[key].blocktime;
-        delete txs[key].blockhash;
-        delete txs[key].time;
-        delete txs[key].isCoinbase;
-        txs[key.toLowerCase()] = txs[key];
-        delete txs[key];
-      }
-      var address = {
-        address: addr,
-        transactions: txs,
-        confirmedReceived: confirmedReceived,
-        unconfirmedReceived: unconfirmedReceived,
-        confirmedPossiblyReceived: confirmedPossiblyReceived,
-        unconfirmedPossiblyReceived: unconfirmedPossiblyReceived
-      };
-      res.render('address', {address: address});
-    });
+  getAddressData(addr, function(address){
+    var response = {};
+    res.render('address', {address: address});
   });
 };
 
+// Returns detailed information about the transactions containing given outputs.
 function getTransactions(outputs, txs, callback){
   var fetched = 0;
   if (outputs.length > 0){
@@ -87,4 +34,81 @@ function getTransactions(outputs, txs, callback){
   else{
     callback(txs);
   }
+}
+
+// Remove transaction data that is not relevant to an address transaction receipt.
+function removeExtraTransactionData(txs){
+  for (var key in txs){
+    delete txs[key].vin;
+    delete txs[key].vout;
+    delete txs[key].scriptSig;
+    delete txs[key].hex;
+    delete txs[key].txid;
+    delete txs[key].version;
+    delete txs[key].locktime;
+    delete txs[key].blocktime;
+    delete txs[key].blockhash;
+    delete txs[key].time;
+    delete txs[key].isCoinbase;
+    txs[key.toLowerCase()] = txs[key];
+    delete txs[key];
+  }
+  return txs;
+}
+
+// Calculates the amount received by an address.
+function calculateReceived(txs){
+  var received = {
+    'confirmed': 0,
+    'unconfirmed': 0,
+    'confirmedPossibly': 0,
+    'unconfirmedPossibly': 0
+  };
+  for (var tx in txs){
+    tx = txs[tx];
+    if (tx.confirmations > config.minConfirmations){
+      if (tx.amount > Math.max(received['confirmed'], received['unconfirmed'])){
+        received['confirmed'] = tx.amount;
+        received['unconfirmed'] = 0;
+      }
+      else{
+        received['confirmedPossibly'] += tx.amount;
+      }
+    }
+    else{
+      if (received['confirmed'] > 0){
+        received['unconfirmedPossibly'] += tx.amount;
+      }
+      else if (received['unconfirmed'] > 0 && tx.amount > received['unconfirmed']){
+        received['unconfirmedPossibly'] = received['unconfirmed'];
+        received['unconfirmed'] = tx.amount;
+      }
+      else{
+        received['unconfirmed'] = tx.amount;
+      }
+    }
+  }
+  return received;
+}
+
+// Get data for an address.
+function getAddressData(addr, callback){
+  var scriptPubKey = (new Address(addr)).getScriptPubKey()['buffer'].toString('hex');
+  var scriptPubKey_hash = crypto.createHash('sha256').update(scriptPubKey).digest('hex');
+  database.get('SELECT HEX(txid) as txid, vout FROM outputs WHERE scriptPubKey_hash=X\''+scriptPubKey_hash+'\'', function(rows){
+    var outputs = rows;
+    getTransactions(outputs, {}, function(txs){
+      var received = calculateReceived(txs);
+      txs = removeExtraTransactionData(txs);
+      var address = {
+        address: addr,
+        confirmedReceived: received['confirmed'],
+        unconfirmedReceived: received['unconfirmed'],
+        confirmedPossiblyReceived: received['confirmedPossibly'],
+        unconfirmedPossiblyReceived: received['unconfirmedPossibly'],
+        transactions: txs
+      };
+      callback(address);
+    });
+  });
 }
