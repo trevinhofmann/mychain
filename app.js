4dc0b790560313b14c32502572f51bafea265532
Trevin Hofmann <trevinhofmann@gmail.com>
2014-10-01 20:50:04 -0500

    Update to require myserver
    This project should now be required by a main server ('myserver') which
    will forward only certain subdomains to the mychain application. Other
    subdomains (such as wallet.mychain.io) can then be forwarded to other
    applications.
    
    Refer to https://github.com/thofmann/myserver

--- app.js
+++ app.js
@@ -1,42 +1,45 @@
-var express = require('express');
-var stylus = require('stylus');
+module.exports = function(){
 
-// Instantiate web server application.
-var app = express();
+  var express = require('express');
+  var stylus = require('stylus');
+  
+  var app = express();
 
-GLOBAL.config = require('./config.json');
-GLOBAL.bitcore = require('bitcore');
-GLOBAL.Address = bitcore.Address;
-GLOBAL.RpcClient = bitcore.RpcClient;
-GLOBAL.rpc = new RpcClient(config.rpc);
-GLOBAL.crypto = require('crypto');
-GLOBAL.AddressTools = require('./lib/AddressTools');
-GLOBAL.TransactionTools = require('./lib/TransactionTools');
-GLOBAL.database = require('./lib/Database');
+  GLOBAL.config = require('./config.json');
+  GLOBAL.bitcore = require('bitcore');
+  GLOBAL.Address = bitcore.Address;
+  GLOBAL.RpcClient = bitcore.RpcClient;
+  GLOBAL.rpc = new RpcClient(config.rpc);
+  GLOBAL.crypto = require('crypto');
+  GLOBAL.AddressTools = require('./lib/AddressTools');
+  GLOBAL.TransactionTools = require('./lib/TransactionTools');
+  GLOBAL.database = require('./lib/Database');
+  GLOBAL.logger = require('./lib/Logger');
 
-// Use 'views' as the directory for HTML views.
-app.set('views', __dirname + '/views');
+  // Use 'views' as the directory for HTML views.
+  app.set('views', __dirname + '/views');
 
-// Use Jade as the HTML view rendering engine.
-app.set('view engine', 'jade');
+  // Use Jade as the HTML view rendering engine.
+  app.set('view engine', 'jade');
 
-// Use the routes.js script to point requests to the appropriate rendering script.
-require('./routes')(app, database);
+  // Log page requests to the console.
+  app.use(logger.log);
 
-// Use Stylus as the CSS middleware.
-app.use(stylus.middleware({
-  src: __dirname + '/public',
-  compile: function(str, path){
-    return stylus(str).set('filename', path);
-  }
-}));
+  // Use the routes.js script to point requests to the appropriate rendering script.
+  require('./routes')(app, database);
 
-// Use morgan as middleware for logging web requests; should replace this with
-// something better that stores log information to a database.
-app.use(require('morgan')('dev'));
+  // Use Stylus as the CSS middleware.
+  app.use(stylus.middleware({
+    src: __dirname + '/public',
+    compile: function(str, path){
+      return stylus(str).set('filename', path);
+    }
+  }));
 
-// Render static files from the 'public' directory.
-app.use(express.static(__dirname + '/public'));
+  // Render static files from the 'public' directory.
+  app.use(express.static(__dirname + '/public'));
 
-// Express begins listening for web requests.
-app.listen(config.port);
+  // Return the app
+  return app;
+
+}
